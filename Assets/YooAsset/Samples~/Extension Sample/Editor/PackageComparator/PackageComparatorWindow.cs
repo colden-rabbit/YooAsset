5f7d1833cb1dc5f35fde5b48f0050e8ddac71bb1
colden-rabbit <2251633709@qq.com>
2026-01-03 20:12:36 +0800

    对比工具增加manifest 解密处理

--- Assets/YooAsset/Samples~/Extension Sample/Editor/PackageComparator/PackageComparatorWindow.cs
+++ Assets/YooAsset/Samples~/Extension Sample/Editor/PackageComparator/PackageComparatorWindow.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEditor;
+using ET;
 
 namespace YooAsset.Editor
 {
@@ -105,12 +106,12 @@ namespace YooAsset.Editor
             newList.Clear();
 
             // 加载补丁清单1
-            byte[] bytesData1 = FileUtility.ReadAllBytes(_manifestPath1);
-            PackageManifest manifest1 = ManifestTools.DeserializeFromBinary(bytesData1, null); //TODO 自行处理解密
+            byte[]          bytesData1 = FileUtility.ReadAllBytes(_manifestPath1);
+            PackageManifest manifest1  = ManifestTools.DeserializeFromBinary(bytesData1, new RestoreManifestServices()); //TODO 自行处理解密
 
             // 加载补丁清单1
-            byte[] bytesData2 = FileUtility.ReadAllBytes(_manifestPath2);
-            PackageManifest manifest2 = ManifestTools.DeserializeFromBinary(bytesData2, null); //TODO 自行处理解密
+            byte[]          bytesData2 = FileUtility.ReadAllBytes(_manifestPath2);
+            PackageManifest manifest2  = ManifestTools.DeserializeFromBinary(bytesData2, new RestoreManifestServices()); //TODO 自行处理解密
 
             // 拷贝文件列表
             foreach (var bundle2 in manifest2.BundleList)
